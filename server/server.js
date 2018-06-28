98d3bb84fdcf80837919aa7cdd6ee5e098315da8
Nedsilon <dragos199993@gmail.com>
2018-06-28 22:18:09 +0300

    added global emit broadcast

--- server/server.js
+++ server/server.js
@@ -1,11 +1,37 @@
 const path = require("path");
+const http = require("http");
+
 const express = require("express");
+const socketIO = require("socket.io");
 
 publicPath = path.join(__dirname, '..', 'public')
 const port = process.env.PORT || 3000;
-const app = express();
 
-app.use(express.static(publicPath))
-app.listen(port, () => {
-    console.log(`listening on port ${publicPath}`);
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    // socket.emit('newMessage', {
+    //     from: 'Mike',
+    //     text: "Hello there",
+    //     createAt: 10
+    // });
+    socket.on('createMessage', (message) => {
+        console.log({message});
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
+    });
+    socket.on('disconnect', () => {
+        console.log("User disconnected");
+    });
+})
+
+app.use(express.static(publicPath));
+server.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
