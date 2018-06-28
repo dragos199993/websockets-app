19fddc4bb762c363333b31d6902e2c8c19250101
Nedsilon <dragos199993@gmail.com>
2018-06-28 22:30:08 +0300

    added global message welcome

--- server/server.js
+++ server/server.js
@@ -4,34 +4,49 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
-publicPath = path.join(__dirname, '..', 'public')
+publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-    console.log('New user connected');
-    // socket.emit('newMessage', {
-    //     from: 'Mike',
-    //     text: "Hello there",
-    //     createAt: 10
+io.on("connection", socket => {
+  console.log("New user connected");
+  // socket.emit('newMessage', {
+  //     from: 'Mike',
+  //     text: "Hello there",
+  //     createAt: 10
+  // });
+  socket.emit("newMessage", {
+    from: "admin",
+    text: "welcome to chat app",
+    createAt: new Date().getTime()
+  });
+  socket.broadcast.emit("newMessage", {
+    from: "admin",
+    text: "new user joined",
+    createAt: new Date().getTime()
+  });
+  socket.on("createMessage", message => {
+    console.log({ message });
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //     from: message.from,
+    //     text: message.text,
+    //     createAt: new Date().getTime()
     // });
-    socket.on('createMessage', (message) => {
-        console.log({message});
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createAt: new Date().getTime()
-        });
-    });
-    socket.on('disconnect', () => {
-        console.log("User disconnected");
-    });
-})
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
 app.use(express.static(publicPath));
 server.listen(port, () => {
-    console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
